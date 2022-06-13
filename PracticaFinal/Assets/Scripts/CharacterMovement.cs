1f300213dc46225df265c736f205ac36d42444a9
Laia <laia.fustercorral@gmail.com>
2022-06-13 18:51:46 +0200

    Basic Character movement

--- PracticaFinal/Assets/Scripts/CharacterMovement.cs
+++ PracticaFinal/Assets/Scripts/CharacterMovement.cs
@@ -6,33 +6,47 @@ public class CharacterMovement : MonoBehaviour
 {
     private CharacterMode characterMode;
     private SpriteRenderer spriteRenderer;
+    private Rigidbody2D rigidbody2D;
+    private float horizontalMovement;
+    private float verticalMovement;
+    private float upSpeed, downSpeed, sideSpeed;
 
     [SerializeField] CharacterMode initialMode;
 
     private void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        sideSpeed = 10;
         ChangeMode(initialMode);
     }
 
     private void Update()
     {
-        if(Input.GetAxis("Vertical") > 0)
+        if(Input.GetAxis("VerticalModeChange") > 0)
         {
             ChangeMode(Side.Top);
         }
-        else if(Input.GetAxis("Vertical") < 0)
+        else if(Input.GetAxis("VerticalModeChange") < 0)
         {
             ChangeMode(Side.Bottom);
         }
-        else if(Input.GetAxis("Horizontal") > 0)
+        else if(Input.GetAxis("HorizontalModeChange") > 0)
         {
             ChangeMode(Side.Right);
         }
-        else if(Input.GetAxis("Horizontal") < 0)
+        else if(Input.GetAxis("HorizontalModeChange") < 0)
         {
             ChangeMode(Side.Left);
         }
+
+        horizontalMovement = Input.GetAxis("HorizontalMovement");
+        verticalMovement = Input.GetAxis("VerticalMovement");
+
+        if(verticalMovement != 0)
+            rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, verticalMovement > 0 ? verticalMovement*upSpeed : verticalMovement*downSpeed);
+        
+        rigidbody2D.velocity = new Vector2(horizontalMovement*sideSpeed, rigidbody2D.velocity.y);
     }
 
     public void ChangeMode(Side side)
@@ -61,15 +75,27 @@ public class CharacterMovement : MonoBehaviour
         {
             case CharacterMode.RubberBall:
                 spriteRenderer.color = Color.red;
+                rigidbody2D.gravityScale = 1;
+                upSpeed = 10;
+                downSpeed = 1;
                 break;
             case CharacterMode.CannonBall:
                 spriteRenderer.color = Color.black;
+                rigidbody2D.gravityScale = 2;
+                upSpeed = 0;
+                downSpeed = 1;
                 break;
             case CharacterMode.Cloud:
                 spriteRenderer.color = Color.white;
+                rigidbody2D.gravityScale = -0.1f;
+                upSpeed = 10;
+                downSpeed = 0.075f;
                 break;
             case CharacterMode.WaterDrop:
                 spriteRenderer.color = Color.cyan;
+                rigidbody2D.gravityScale = 1;
+                upSpeed = 0;
+                downSpeed = 1;
                 break;
         }
     }
